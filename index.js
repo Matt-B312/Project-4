dddb25d6066e4beddd1470b7a78a9bb0b17abc44
Matthew Lebreton <mlebreton84813@gmail.com>
2022-04-28 19:26:59 -0400

    new changes

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
+import favicon from 'serve-favicon';
+import path from 'path';
 
+const __dirname = path.resolve();
 const app = express();
 dotenv.config();
 
-
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
@@ -18,8 +22,11 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('APP IS RUNNING.');
+// app.get('/', (req, res) => {
+//     res.send('APP IS RUNNING.');
+// });
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
