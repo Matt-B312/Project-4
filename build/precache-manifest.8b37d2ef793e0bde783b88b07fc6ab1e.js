0fa59361fa3eca859151c03b87479a422921a81a
Matthew Lebreton <mlebreton84813@gmail.com>
2022-04-28 18:19:23 -0400

    make it better

---
+++ build/precache-manifest.8b37d2ef793e0bde783b88b07fc6ab1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b93c34de29c3582e1fe22395eb53bd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9075590deb6c2a8555",
+    "url": "/static/css/main.cd64aaef.chunk.css"
+  },
+  {
+    "revision": "04119f96d02d26028719",
+    "url": "/static/js/2.3ff2c9b0.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.3ff2c9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9075590deb6c2a8555",
+    "url": "/static/js/main.17d45d09.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/static/media/memoriesLogo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/static/media/memoriesText.18ebdb6d.png"
+  }
+]);
